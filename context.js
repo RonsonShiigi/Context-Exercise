210a9ca1bc2bdfa4deb4ba1ac9cf8c38d9ccff50
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2019-01-21 13:13:16 -1000

    pau

--- context.js
+++ context.js
@@ -15,16 +15,19 @@ var megaman = new Robot("Mega Man");
 var bmo = new Robot("B-Mo");
 var wall_e = new Robot("Wall-E");
 
+let calculate = add.bind(bender)
+
 
 // #1
-console.log(  );
+
+console.log( add(0,1) );
 
 // #2  
-console.log(  );
+console.log( add.call(voltron,2,3) );
 
 // #3
-console.log(  );
+console.log( add.apply(optimus,[20,30]) );
 
 // #4
 
-console.log(  );
+console.log(calculate('drinking ','beer'))
